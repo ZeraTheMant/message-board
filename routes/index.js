413efc65498eb8a6228acc1951ff16fb14056dd9
unknown <imbapalagz@gmail.com>
2021-05-20 09:35:39 +0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+const messages = [
+	{
+		text: "Hi there!",
+		user: "Amando",
+		added: new Date()
+	},
+	{
+		text: "Hello World!",
+		user: "Charles",
+		added: new Date()
+	}
+];
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  const context = {
+	title: "Mini Messageboard",
+	messages: messages
+  }
+  res.render('index', context);
+});
+
+module.exports = router;
